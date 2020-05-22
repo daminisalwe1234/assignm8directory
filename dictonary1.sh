d99e0316569c66e923a5eafc633973d8d8d7a55e
daminisalwe1234 <salwedamini2gmail.com>
2020-05-22 06:32:31 +0530

    dictionary2

--- dictonary1.sh
+++ dictonary1.sh
@@ -1,38 +1,15 @@
 #!/bin/bash 
-declare -A Dice
-for(( count=1;count<=6;count++ ))
+declare -A birthMonth
+for (( count=1; count<=50; count++ ))
 do
-	Dice["Face"$count]=0
-done
-loopStop=0
-while [ $loopStop -ne 1 ]
-do
-   randomNumber=$(( RANDOM%6 + 1 ))
-   ((Dice["Face"$randomNumber]++))
-   if [ ${Dice["Face"$randomNumber]} -eq 10 ]
-   then
-      (( loopStop++ ))
-   fi
-done
-maximum=${Dice[Face1]}
-minimum=${Dice[Face1]}
-faceMax=1
-faceMin=1
-for (( count=1;count<=6;count++ ))
-do
-   if [ $maximum -lt ${Dice["Face"$count]} ]
-   then
-
-      maximum=${Dice["Face"$count]}
-      faceMax=$count
-   elif [ $minimum -gt ${Dice["Face"$count]} ]
-   then
-      minimum=${Dice["Face"$count]}
-      faceMin=$count
-   fi
+var1=$((RANDOM%24 + 1 )) 
+if [ $var1 -lt 13 ]
+then
+var1+=\-92;
+else
+var1+=\-93;
+fi
+for
 
 done
-
-echo "MAXIMUM OCCURRENCE Face$faceMax $maximum times"
-echo "MINIMUM OCCURRENCE Face$faceMin $minimum times"
 
